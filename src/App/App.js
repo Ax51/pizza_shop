21cc9e47ce00d4eb0c945cd5e057a51783694ee5
Ax51 <80718359+Ax51@users.noreply.github.com>
2021-12-09 00:17:09 +0300

    initial commit

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import logo from '../img/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Pizza shop</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
